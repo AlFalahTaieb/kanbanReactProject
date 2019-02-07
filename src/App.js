71db52526421fd87482125e2392f54d1ba22465d
AlFalah <taiebalfalah@gmail.com>
2019-02-07 10:02:31 +0100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,81 +1,24 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Router, Route, Switch } from 'react-router-dom'
 
-// const signup = "/signup"
+import Login from './components/LoginPage/LoginPage'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      username: '',
-      password: '',
-      responseToPost: '',
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-
-  }
-  handleSubmit = async e => {
-    e.preventDefault()
-    const response = await fetch("/signup", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username: this.state.username, password: this.state.username }),
-    })
-    const body = await response.text()
-    console.log(this.state.username)
-    console.log(this.state.password)
-    this.setState({ responseToPost: body })
-  }
-
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value })
-  }
-
-  render() {
-    const { username, password, submitted, loading, error } = this.state;
+class App extends Component{
+  render(){
     return (
-      <div className="col-md-6 col-md-offset-3">
-        <div className="alert alert-info">
-          Username: taieb<br />
-          Password: password:D
-                </div>
-        <h2>Login</h2>
-        <form name="form" onSubmit={this.handleSubmit}>
-          <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-            <label htmlFor="username">Username</label>
-            <input type="text" className="form-control" name="username" value={this.state.username} onChange={e => this.setState({ username: e.target.value })} />
-            {submitted && !username &&
-              <div className="help-block">Username is required</div>
-            }
-          </div>
-          <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" name="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })} />
-            {submitted && !password &&
-              <div className="help-block">Password is required</div>
-            }
-          </div>
-          <div className="form-group">
-            <button className="btn btn-primary" disabled={loading}>Login</button>
-            {loading &&
-              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-            }
-          </div>
-          {error &&
-            <div className={'alert alert-danger'}>{error}</div>
-          }
-        </form>
 
-        <p>{this.state.responseToPost}</p>
+      <BrowserRouter>
+      <div className="App">
+      <Switch>
+        <Route exact path="/" component={Login}/>
+      </Switch>
+
       </div>
-    );
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
